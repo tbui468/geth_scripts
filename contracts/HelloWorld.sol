89d95c88622ec688dbd209ef1a186e445f9963d8
thomas <thomasthienbui@gmail.com>
2022-11-29 10:41:09 +0700

    Refactor script to make sending json-rpc not require strings for arguments

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -3,8 +3,19 @@
 pragma solidity ^0.8.17;
 
 contract HelloWorld {
-    uint256 public s = 42;
-    function get() public view returns (uint256) {
-        return s;
+    string public s = "Hello world";
+    uint256 public n = 42;
+
+    function add(uint _a, uint _b) public pure returns (uint256) {
+        return _a + _b;
+    }
+
+    function setN(uint _n) public pure returns (uint256) {
+        uint256 t = _n;
+        return t;
+    }
+
+    function setS(string calldata _s) public {
+        s = _s;
     }
 }
--- deploy_contract.py
+++ deploy_contract.py
@@ -3,59 +3,81 @@ import time
 import json
 from eth_utils import keccak
 
-def send_ipc_request(method, params, id_, node):
-    p = subprocess.Popen('echo \'{"jsonrpc":"2.0","method":"' + method + '","params":' + params + ',"id":' + id_ + '}e\' | nc -U ./' + node + '/geth.ipc', shell=True, stdout=subprocess.PIPE)
+id_count = 0
+signer = "node2"
+
+#Could make a send_request_and_wait function that loops until a receipt is received
+
+def send_ipc_request(method, params):
+    global id_count, signer
+    id_count += 1
+
+    param_string = "["
+    for i in range(len(params)):
+        if isinstance(params[i], dict):
+            param_string += json.dumps(params[i])
+        else:
+            param_string += "\"" + params[i] + "\""
+
+        if i != len(params) - 1:
+            param_string += ","
+
+    param_string += "]"
+
+
+    p = subprocess.Popen('echo \'{"jsonrpc":"2.0","method":"' + method + '","params":' + param_string + ',"id":' + str(id_count) + '}e\' | nc -U ./' + signer + '/geth.ipc', shell=True, stdout=subprocess.PIPE)
     output = str(p.stdout.read()).split('\\n')
     return json.loads(output[1])
+
+#test input
 
 #compile contract
 subprocess.run("cd contracts && ./solc HelloWorld.sol --bin -o . --overwrite --abi", shell=True)
 
-coinbase = send_ipc_request("eth_coinbase", "[]", "1", "node2")
-balance = send_ipc_request("eth_getBalance", '["' + coinbase['result'] + '", "latest"]', "2", "node2")
-gas_price = send_ipc_request("eth_gasPrice", '[]', "3", "node2")
+coinbase = send_ipc_request("eth_coinbase", [])
+balance = send_ipc_request("eth_getBalance", [coinbase['result'], "latest"])
+gas_price = send_ipc_request("eth_gasPrice", [])
 print(coinbase)
 print(balance)
 print(gas_price)
 
-
-binary = ""
+binary = "0x"
 with open("./contracts/HelloWorld.bin", "r") as f:
-    binary = str(f.read())
+    binary += str(f.read())
 
-print(binary)
-
-gas_est = send_ipc_request("eth_estimateGas", '[{"from": "' + coinbase['result'] + '", "data": "0x' + binary + '"}]', "4", "node2")
+gas_est = send_ipc_request("eth_estimateGas", [{"from": coinbase['result'], "data": binary}])
 print(gas_est)
 
 
-trans_hash = send_ipc_request("eth_sendTransaction", '[{"from": "' + coinbase['result'] + '", "gas": "' + gas_est['result'] + '", "data": "0x' + binary + '"}]', "5", "node2")
+trans_hash = send_ipc_request("eth_sendTransaction", [{"from": coinbase['result'], "gas": gas_est['result'], "data": binary}])
 print(trans_hash)
 
 while True:
-    trans_receipt = send_ipc_request("eth_getTransactionReceipt", '["' + trans_hash['result'] + '"]', "7", "node2")
+    trans_receipt = send_ipc_request("eth_getTransactionReceipt", [trans_hash['result']])
     if trans_receipt['result'] != None:
         print(trans_receipt)
         break
 
+data = "0x" + str(keccak(text='setN(uint256)').hex())[0:8]
+data += "0" * 62 + "01"
+gas_est = send_ipc_request("eth_estimateGas", [{"from": coinbase['result'], "to": trans_receipt['result']['contractAddress'], "data": data}])
+print(gas_est)
 
-data = "0x" + str(keccak(text='get()').hex())[0:8]
-print(data)
 
-
-"""
-call_hash = send_ipc_request("eth_sendTransaction", '[{"from": "' + coinbase['result'] + '", "to": "' + trans_receipt['result']['contractAddress'] +  '", "data": "' + data + '"}]', "8", "node2")
+call_hash = send_ipc_request("eth_sendTransaction", [{"from": coinbase['result'], "gas": gas_est['result'], "to": trans_receipt['result']['contractAddress'], "data": data}])
 print(call_hash)
 
 while True:
-    call_receipt = send_ipc_request("eth_getTransactionReceipt", '["' + call_hash['result'] + '"]', "9", "node2")
+    call_receipt = send_ipc_request("eth_getTransactionReceipt", [call_hash['result']])
     if call_receipt['result'] != None:
         print(call_receipt)
         break
-"""
 
-call_result = send_ipc_request("eth_call", '[{"to": "' + trans_receipt['result']['contractAddress'] +  '", "data": "' + data + '"},  "latest"]', "8", "node2")
+"""
+data = "0x" + str(keccak(text='n()').hex())[0:8]
+call_result = send_ipc_request("eth_call", '[{"to": "' + trans_receipt['result']['contractAddress'] +  '", "data": "' + data + '"},  "latest"]')
 print(call_result)
+"""
 
 #Use ABI to encode the payload
 #Send a transaction with the payload and check result (use ABI to decode)
